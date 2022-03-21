7ba9321e245a079d304a68ab2b15913fab8d7124
Your Name <you@example.com>
2022-03-20 20:04:08 -0700

    left side tree of questions

--- index.js
+++ index.js
@@ -9,6 +9,30 @@
     On the other side of him, you see a shiny chest of treasure. Another path would 
     lead you away from the dragon altogether. Which path do you take?`)
     }
+     if(firstAnswer === 'Follow'){
+         let secondAnswer = window.prompt('You follow the cat to a colony of cats, 
+         nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. 
+         They are content with you staying, but you wonder if you should alert the world to 
+         this magical safe haven.')
 
+     }else if(firstAnswer === 'Continue'){
+        let secondAnswer = window.prompt('You come across a chamber that reaches upward to a 
+        shining light above. There is a long, winding staircase, and a much quicker, but 
+        rickety-looking ladder that leads up toward the light. Which do you take?')
+    }
+    
+    if(firstAnswer === 'Stay'){
+        let secondAnswer = window.prompt(You live happily amongst the cats for the rest of your days.')
 
+    } else  if(firstAnswer === 'Spread the word.')
+        let secondAnswer= window.prompt('After leaving the cat colony, you are never able to find it 
+        again; without proof, no one believes your story, which passes into legend nonetheless.')
+    }
+    if(firstAnswer === 'ladder'){
+        let secondAnswer = window.prompt('After ascending a few feet up the ladder, one of its rungs snaps,
+         and you comedically fall through each of the rungs below. Sheepish, you return home.')
+    } else if(firstAnswer === 'Staircase')
+        let secondAnswer = window.prompt ('After ascending a few feet up the ladder, one of its 
+        rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+}
 //Your Code here
